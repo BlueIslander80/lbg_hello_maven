bc8b4847a0d7dc18e69b60d9e4b6404854035767
Simon <simonbarker1980@gmail.com>
2026-03-10 15:26:48 +0000

    EDIT: Hello everyone version 1.2

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -14,6 +14,7 @@ public class App
         System.out.println(sayHelloToSomeone("Charlie"));
         System.out.println(sayHelloToSomeone("Zena"));
         System.out.println(sayHelloToSomeone("RodStewart"));
+        System.out.println(sayHelloToSomeone("tomyrowofpeople"));
     }
 
     public static String sayHello(){
